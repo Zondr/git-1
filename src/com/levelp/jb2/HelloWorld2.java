0bb4d18d25a61d74c7868a3c3beb7bd7c2849238
Evgeny Alelekov <zondr7@gmail.com>
2018-02-05 21:08:29 +0700

    два файла

--- src/com/levelp/jb2/HelloWorld2.java
+++ src/com/levelp/jb2/HelloWorld2.java
@@ -2,6 +2,6 @@ package com.levelp.jb2;
 
 public class HelloWorld2 {
     public static void main(String[] args){
-        System.out.println("HelloWorld2");
+        System.out.println("Hello World2");
     }
 }
